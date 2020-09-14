fc47896d31f7c84fdcae082fb758bec78f05439f
Matt Sim <simzzang85@gmail.com>
2020-09-14 15:51:04 -0500

    changings

--- clock.js
+++ clock.js
@@ -21,7 +21,7 @@ function drawFace(ctx, radius) {
   ctx.fill();
   grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
   grad.addColorStop(0, '#333');
-  grad.addColorStop(0.5, 'white');
+  grad.addColorStop(0.5, '#333');
   grad.addColorStop(1, '#333');
   ctx.strokeStyle = grad;
   ctx.lineWidth = radius*0.1;
@@ -98,5 +98,5 @@ function workTime(hour,min, sec){
     if(rtnMsg != ""){
         alert(rtnMsg);
     }
-    return rtnMsg;
+    
 }
